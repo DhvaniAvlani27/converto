b895bf8140f293b493387bb571d73f27d1170547
DhvaniAvlani27 <dhvaniavlani27@gmail.com>
2025-08-25 00:56:01 +0530

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = { //What is this 
   output: 'standalone',
   experimental: {
     outputFileTracingRoot: undefined,
   },
 };
 
-export default nextConfig;
+export default nextConfig;    
