5099c916b2a6c238907bb237a3e188bcfaf1b1d2
DhvaniAvlani27 <dhvaniavlani27@gmail.com>
2025-08-25 10:24:35 +0530

    addvgdddv

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
     <header className="bg-gradient-to-r from-gray-700 to-gray-900 text-white p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
       <div className="text-2xl font-extrabold tracking-widest bg-clip-text text-transparent bg-gradient-to-r from-pink-400 to-purple-500">
-            Converto - JPG to PDF</div>
+            Converto - JPG to WORD</div>
         <div className="ml-6 text-sm text-gray-300">© 2024 by HP Creates</div>
       </div>
     </header>
